35064f2ce28cebabefc9350456a6941e23ae7ac2
myisjack99 <myisjack99@gmail.com>
2022-09-18 21:30:24 +0800

    修正VR裝置skybox顯示問題、遮蔽PC看見VR手部、VR長按偵測功能

--- Assets/Game/Script/VR_CheckLongPress.cs
+++ Assets/Game/Script/VR_CheckLongPress.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class VR_CheckLongPress : MonoBehaviour
 {
+    public SteamVR_Action_Boolean inputAction;
+
     protected bool longPress = false;
     [SerializeField]
     public float pressTriggerTime = 2.0f;
@@ -11,7 +14,8 @@ public class VR_CheckLongPress : MonoBehaviour
 
     public bool CheckButton()
     {
-        if (Input.GetButton("XRI_Right_TriggerButton") || Input.GetButton("XRI_Right_GripButton"))
+        //if (Input.GetButton("XRI_Right_Trigger") || Input.GetButton("XRI_Right_Grip"))
+        if (inputAction.GetState(SteamVR_Input_Sources.RightHand))
         {
             pressTime += Time.deltaTime;
             if (pressTime >= pressTriggerTime)
