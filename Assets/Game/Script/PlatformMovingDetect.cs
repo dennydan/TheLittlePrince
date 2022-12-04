02e5516c8fab287105d4257af4a3df3e38a0bf3e
myisjack99 <myisjack99@gmail.com>
2022-12-04 16:11:00 +0800

    小狐狸透過平台移動以及補充patch: 砍樹動畫觸發
    動畫加速、動畫觸發砍樹
    平台移動問題修正

--- Assets/Game/Script/PlatformMovingDetect.cs
+++ Assets/Game/Script/PlatformMovingDetect.cs
@@ -5,20 +5,47 @@ using Gamekit3D.GameCommands;
 
 public class PlatformMovingDetect : MonoBehaviour
 {
-    public Vector3 originPosition, deltaPosition;
-    protected Platform platform;
+    public Vector3 originPosition, deltaPosition, newposition;
+    //public Quaternion origiRotation, deltaRotation, newRotation;
+    public Platform platform;
+
+    protected CharacterController m_CharacterController;
 
     private void Start()
     {
-        platform = GetComponentInChildren<Platform>();
+        m_CharacterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        deltaPosition = transform.position - originPosition;
-        originPosition = transform.position;
-        //Debug.Log("plat: "+platform.transform.name);
-        platform.MoveCharacterController(deltaPosition);
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            platform = other.GetComponent<Platform>();
+            originPosition = platform.transform.position;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            if (platform != null)
+            {
+                newposition = platform.transform.position;
+                deltaPosition = newposition - originPosition;
+                originPosition = newposition;
+                Debug.Log("move: " + deltaPosition);
+                m_CharacterController.Move(deltaPosition);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            platform = null;
+            originPosition = deltaPosition  = newposition = new Vector3(0,0,0);
+        }
     }
 }
